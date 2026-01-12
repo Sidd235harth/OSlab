d5347f2d8bd69fa34eb507d2cb877c4b062e9117
Siddharth TR <siddartha.24.beai@acharya.ac.in>
2026-01-12 22:10:12 +0530

    Add first fit memory management program
    Implement first fit memory management scheme in C.

---
+++ oslab6c.c
@@ -0,0 +1,53 @@
+//first fir
+#include <stdio.h>
+#define max 25
+
+int main() {
+    int frag[max], b[max], f[max];
+    int bf[max] = {0}, ff[max];
+    int i, j, nb, nf, temp;
+
+    printf("\nMemory Management Scheme - First Fit\n");
+
+    printf("Enter number of blocks: ");
+    scanf("%d", &nb);
+
+    printf("Enter number of files: ");
+    scanf("%d", &nf);
+
+    for (i = 0; i < nb; i++) {
+        printf("Block %d size: ", i + 1);
+        scanf("%d", &b[i]);
+    }
+
+    for (i = 0; i < nf; i++) {
+        printf("File %d size: ", i + 1);
+        scanf("%d", &f[i]);
+        ff[i] = -1;
+    }
+
+    for (i = 0; i < nf; i++) {
+        for (j = 0; j < nb; j++) {
+            if (!bf[j]) {
+                temp = b[j] - f[i];
+                if (temp >= 0) {
+                    ff[i] = j;
+                    frag[i] = temp;
+                    bf[j] = 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("\nFile\tSize\tBlock\tBlock Size\tFragment");
+    for (i = 0; i < nf; i++) {
+        if (ff[i] != -1)
+            printf("\n%d\t%d\t%d\t%d\t\t%d",
+                   i + 1, f[i], ff[i] + 1, b[ff[i]], frag[i]);
+        else
+            printf("\n%d\t%d\tNot Allocated", i + 1, f[i]);
+    }
+
+    return 0;
+}
